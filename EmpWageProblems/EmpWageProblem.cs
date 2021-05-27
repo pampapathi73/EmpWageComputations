4f41de41cddc64a6a1792b14de52962093509187
pampapathi73 <pampapathik32@gmail.com>
2021-05-27 21:01:38 +0530

    [pampapathi]  added: calculate daily employee wage

--- EmpWageProblems/EmpWageProblem.cs
+++ EmpWageProblems/EmpWageProblem.cs
@@ -7,15 +7,29 @@ namespace EmpWageProblems
     class EmpWageProblem
     {
         readonly int IS_PRESENT = 1;
+        readonly int Wage_Per_Hours = 20;
+        readonly int Full_Day_Hour = 8;
 
         public void Attendance()
         {
             Random random = new Random();
             int empCheck = random.Next(0, 2);
             if (empCheck == IS_PRESENT)
+            {
                 Console.WriteLine("Employee is Present");
+                CalculateDailyWage();
+            }
             else
+            {
                 Console.WriteLine("Employee is Absent");
+                Console.WriteLine("Employee Wage is null");
+            }
+
+        }
+        public void CalculateDailyWage()
+        {
+            int EmployeeWage = Wage_Per_Hours * Full_Day_Hour;
+            Console.WriteLine("Employee Full Wage is : " + EmployeeWage);
 
         }
         }
