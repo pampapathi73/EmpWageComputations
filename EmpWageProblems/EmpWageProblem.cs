377ab98c57d060b6ec621a89f1a998fbe97da69e
pampapathi73 <pampapathik32@gmail.com>
2021-05-27 19:23:11 +0530

    [pampapathi] add: welcome to employee wage problem

---
+++ EmpWageProblems/EmpWageProblem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmpWageProblems
+{
+    class EmpWageProblem
+    {
+        readonly int IS_PRESENT = 1;
+
+        public void Attendance()
+        {
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_PRESENT)
+                Console.WriteLine("Employee is Present");
+            else
+                Console.WriteLine("Employee is Absent");
+
+        }
+        }
+    }
---
+++ EmpWageProblems/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EmpWageProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Program"); //main
+            EmpWageProblem EmployeeWage = new EmpWageProblem();
+            EmployeeWage.Attendance();
+            Console.ReadKey();
+        }
+    }
+}
